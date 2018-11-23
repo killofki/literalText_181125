725251e558b2a6c7a344c4ab35eaea8634661c7a
killofki <killofki@yahoo.co.kr>
2018-11-24 02:39:09 +0900

    with text

--- heyguy.js
+++ heyguy.js
@@ -3,7 +3,7 @@ console .log( 이보게젊은이 `
 **** ${ 
 
 ( 이보게, 그것이, 무엇인지, 네, 사전, 이거, 무슨, 그거 ) => ` 
-“${ 이보게 }, ${ 그것이 } 이 ${ 무엇인지 } ${ 무엇인지 }?” 
+“${ 이보게 }, ${ 그것이 }이 ${ 무엇인지 } ${ 무엇인지 }?” 
 “${ 네 }?” 
 “${ 그것이 }${ 무엇인지 }.” 
 ${ 이보게 }, ${ 사전 }. 
@@ -13,34 +13,36 @@ ${ 이보게 }, ${ 사전 }.
 
 }
 
-이보게 ${ 있는대로만 `${ [ '이보게 ' ] }젊은이` } 
-그것이 ${ 있는대로만 `${ [ '요즘 ' ] }유행하는 셈틀 선물거래 응용프로그램` } 
-무엇인지 ${ 있는대로만 `${ [ '무엇인지', '알려줄 수 있겠나', '말일세' ] }` } 
-네 ${ 있는대로만 `${ [ '네', '말씀하시는거죠' ] }` } 
+이보게 ${ 글자로뽑기( `[이보게 ]젊은이` ) } 
+그것이 ${ 글자로뽑기( `[요즘 ]유행하는 셈틀 선물거래 응용프로그램` ) } 
+무엇인지 ${ 글자로뽑기( `[무엇인지,알려줄 수 있겠나,말일세]` ) } 
+네 ${ 글자로뽑기( `[네,말씀하시는거죠]` ) } 
 사전 ${ '사전을 뒤진다' } 
 이거 ${ 'desktop version futures trading application' } 
-무슨 ${ 있는대로만 `${ [ '무슨 말을 하는겐가', '모르겠네만' ] }` } 
+무슨 ${ 글자로뽑기( `[무슨 말을 하는겐가,모르겠네만]` ) } 
 그거 ${ '꼬부랑말' } 
 
 	` ); 
 
 function 이보게젊은이( 널널한공간, 받아적어, ... 거시기 ) { return 받아적어( ... 거시기 ); } 
 function 있는대로만( { raw }, ... ar ) { 
-let printIndex = -1; return { toString : q => 
+	let printIndex = -1; return { toString : q => 
 	String .raw( { raw }, ( 
-		  printIndex += 1
-		, ar .map( a => a[ printIndex ] || '' ) 
+		  printIndex += 1 
+		, ar .map( a => a && a[ printIndex ] || '' ) 
 		) ) 
 	}; } 
 function ttoraw( t, regv, regF ) { 
 	let raw = [], ar = []; 
 	t .replace( regv, regF( raw, ar ) ); 
-	let rpop = raw .pop(); 
+	let rpop, apop; 
+	while ( [ rpop = raw .pop(), apop = ar .pop() ] .every( v => ! v ) ) { 
+		} 
+	  apop ? ( raw .push( rpop, '' ), ar .push ( apop ) ) 
+	: rpop && ( raw .push( rpop ), ar .push ( apop ) ) 
+		; 
 	raw .raw = raw; 
-	raw .push( rpop ); 
-	rpop ? raw .push( '' ) 
-		: ar .length = raw .length - 1  // final match, finish match 
-		;  
+	ar .length = raw .length - 1; 
 	return [ raw, ... ar ]; 
 	} 
 function 글자로뽑기( t ) { 
@@ -48,7 +50,7 @@ function 글자로뽑기( t ) {
 		, /([^[]*)(\[([^\]]*?)\])?/g 
 		, ( raw, ar ) => ( all, rawv, arv, arvv ) => ( 
 			  raw .push( rawv ) 
-			, ar .push( arv && arvv ) 
+			, ar .push( arv && arvv .split( ',' ) ) 
 			) 
 		) ); 
 	} 
