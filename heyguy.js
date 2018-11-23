e2aeee74a3266676c33156abea20175e70f57d47
killofki <killofki@yahoo.co.kr>
2018-11-24 03:06:17 +0900

    ready object

--- heyguy.js
+++ heyguy.js
@@ -2,7 +2,7 @@ console .log( 이보게젊은이 `
 
 **** ${ 
 
-( 이보게, 그것이, 무엇인지, 네, 사전, 이거, 무슨, 그거 ) => ` 
+( 블라블라, 이보게, 그것이, 무엇인지, 네, 사전, 이거, 무슨, 그거 ) => ` 
 “${ 이보게 }, ${ 그것이 }이 ${ 무엇인지 } ${ 무엇인지 }?” 
 “${ 네 }?” 
 “${ 그것이 }${ 무엇인지 }.” 
@@ -24,7 +24,9 @@ ${ 이보게 }, ${ 사전 }.
 
 	` ); 
 
-function 이보게젊은이( 널널한공간, 받아적어, ... 거시기 ) { return 받아적어( ... 거시기 ); } 
+function 이보게젊은이( 널널한공간, 받아적어, ... 거시기 ) { 
+	return 받아적어( Object .assign( ... 널널한공간 .map( t => 범위로뽑기( t ) ) ), ... 거시기 ); 
+	} 
 function 있는대로만( { raw }, ... ar ) { 
 	let printIndex = -1; return { toString : q => 
 	String .raw( { raw }, ( 
@@ -36,13 +38,13 @@ function ttoraw( t, regv, regF ) {
 	let raw = [], ar = []; 
 	t .replace( regv, regF( raw, ar ) ); 
 	let rpop, apop; 
-	while ( [ rpop = raw .pop(), apop = ar .pop() ] .every( v => ! v ) ) { 
+	while ( [ rpop = raw .pop(), apop = ar .pop() ] .every( v => ! v ) && raw .length ) { 
 		} 
 	  apop ? ( raw .push( rpop, '' ), ar .push ( apop ) ) 
 	: rpop && ( raw .push( rpop ), ar .push ( apop ) ) 
 		; 
 	raw .raw = raw; 
-	ar .length = raw .length - 1; 
+	raw .length ? ( ar .length = raw .length - 1 ) : raw .push( '' ); 
 	return [ raw, ... ar ]; 
 	} 
 function 글자로뽑기( t ) { 
@@ -56,13 +58,16 @@ function 글자로뽑기( t ) {
 	} 
 function 범위로뽑기( t ) { 
 	let [ raw, ... ar ] = ttoraw( t 
-		, /\s*([^{]*?)\s*(\{([^}]*?)\})/g 
+		, /\s*([^{]*)\s*(\{([^}]*?)\})/g 
 		, ( raw, ar ) => ( all, rawv, arv, arvv ) => ( 
 			  raw .push( rawv ) 
 			, ar .push( arv && arvv ) 
 			) 
 		); 
-	return Object .assign( ... ar .map( ( t, p ) => ({ [ raw[ p ] ] : 글자로뽑기( t ) }) ) ); 
+	return ar .length ? Object .assign( ... ar .map( ( t, p ) => 
+			({ [ raw[ p ] ] : 글자로뽑기( t ) }) ) 
+			) 
+		: {}; 
 	} 
 
 // idea from https://twitter.com/Ranol__/status/1065972494060871680 
