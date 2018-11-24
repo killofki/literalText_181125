a0e53af4989e505d00511b82d36cfcddc4a2db66
killofki <killofki@yahoo.co.kr>
2018-11-24 18:23:59 +0900

    erase console other

--- heyguy.js
+++ heyguy.js
@@ -29,10 +29,8 @@ ${ 이보게 }, ${ 사전 }.
 
 var 받아왔어요; 
 function 이보게젊은이( 널널한공간, 받아적어, ... 거시기 ) { 
-	console .log( JSON .stringify( 받아왔어요 ) ); 
 	Object .assign( 받아왔어요 = 받아왔어요 || {}, ... 널널한공간 .map( t => 범위로뽑기( t ) ) ); 
 	거시기 = 거시기 .map( v => v instanceof Function ? v( 받아왔어요 ) : v ); 
-	console .log( JSON .stringify( 받아왔어요 ) ); 
 	return 받아적어( 받아왔어요, ... 거시기 ); 
 	} 
 function 대략( ... ar ) { 
