40234f82340689944d879536cbd730996a6909c4
killofki <killofki@yahoo.co.kr>
2018-11-24 20:17:20 +0900

    viral with

--- heyguy.js
+++ heyguy.js
@@ -27,6 +27,7 @@ ${ 이보게 }, ${ 사전 }.
 
 	` ); 
 // idea from https://twitter.com/Ranol__/status/1065972494060871680 
+// viral with https://twitter.com/killofki/status/1066035649134845952 
 
 function initConst() { 
 	return { 이보게젊은이, 대략 }; 
