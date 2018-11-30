c0f0ff9dcd2927733e79e8f960d5d6f452d89e5c
killofki <killofki@yahoo.co.kr>
2018-12-01 05:58:25 +0900

    defende about error

--- heyguyBase.js
+++ heyguyBase.js
@@ -2,6 +2,7 @@
 // idea from https://twitter.com/Ranol__/status/1065972494060871680 
 // viral with https://twitter.com/killofki/status/1066035649134845952 
 
+var module = module || {}; 
 module .exports = { ... heyguyBase() }; 
 
 function heyguyBase() { 
