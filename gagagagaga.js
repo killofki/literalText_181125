79f0de063df06cdf28ded5e3f9765424d81f514f
killofki <killofki@yahoo.co.kr>
2020-02-08 11:43:44 +0900

    assign

--- gagagagaga.js
+++ gagagagaga.js
@@ -17,22 +17,24 @@ console .log( 이보게젊은이 `
 
 function initConst() { 
 ///// 
+	let { assign, create } = Object 
+	
 	return { 이보게젊은이, 대략, 대충 } 
 
 // functions.. 
 var 받아왔어요 
 function 이보게젊은이( 널널한공간, 받아적어, ... 거시기 ) { 
 	let 남은값 = [] 
-	Object .assign( 
+	assign( 
 		  받아왔어요 = 받아왔어요 || {} 
 		, ... 널널한공간 .map( t => 범위로뽑기( t, 남은값 ) ) 
 		) 
 	let [ 시작값 ] = 남은값 
-	시작값 && Object .assign( 
+	시작값 && assign( 
 		  받아왔어요 
 		, { [ 시작값 ] : 받아적어 } 
 		) // remain original function 
-	Object .assign( 
+	assign( 
 		  받아왔어요 
 		, ... 거시기 .map( ( v, i ) => 
 			({ [ 남은값[ i + 1 ] ] : ForV( v ) }) // extract one time 
@@ -48,7 +50,7 @@ function 대략( ... ar ) {
 	return { toString } 
 	
 	function toString() { // lazy loader with fix value .. 
-		return `${ Object .assign( this, 글자로뽑기( `${ 대충( ... ar ) }` ) ) }` 
+		return `${ assign( this, 글자로뽑기( `${ 대충( ... ar ) }` ) ) }` 
 		} // -- toString() < 대략() 
 	} // -- 대략() 
 
@@ -80,7 +82,7 @@ function 대충( raw, ... ar ) {
 				) 
 			} 
 		ara .pop() // cut about pack 
-		rawa = Object .assign( Object .create( rawa ), new class { 
+		rawa = assign( create( rawa ), new class { 
 			raw = rawa 
 			} ) 
 		
@@ -141,7 +143,7 @@ function 범위로뽑기( t, 남은값 ) {
 			) 
 		) 
 	남은값 .push( raw[ ar .length ] ) 
-	return ar .length ? Object .assign( ... ar .map( ( t, p ) => 
+	return ar .length ? assign( ... ar .map( ( t, p ) => 
 			({ [ raw[ p ] ] : 글자로뽑기( t ) }) ) 
 			) 
 		: {} 
