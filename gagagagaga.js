25f0592ccdc6c1194d814a8832a7a49ec8f51575
killofki <killofki@yahoo.co.kr>
2020-02-08 11:11:40 +0900

    erase semicolon

--- gagagagaga.js
+++ gagagagaga.js
@@ -11,75 +11,82 @@ console .log( 이보게젊은이 `
 성씨 가 { 가〮 } 
 인가요 가 { 가 } 
 
-	` ); 
+	` ) 
 // idea from https://twitter.com/Ranol__/status/1065972494060871680 
 // viral with https://twitter.com/killofki/status/1066035649134845952 
 
 function initConst() { 
 ///// 
-	return { 이보게젊은이, 대략, 대충 }; 
+	return { 이보게젊은이, 대략, 대충 } 
 
 // functions.. 
-var 받아왔어요; 
+var 받아왔어요 
 function 이보게젊은이( 널널한공간, 받아적어, ... 거시기 ) { 
-	let 남은값 = []; 
-	Object .assign( 받아왔어요 = 받아왔어요 || {}, ... 널널한공간 .map( t => 범위로뽑기( t, 남은값 ) ) ); 
-	let [ 시작값 ] = 남은값; 
-	시작값 && Object .assign( 받아왔어요, { [ 시작값 ] : 받아적어 } ); // remain original function 
+	let 남은값 = [] 
+	Object .assign( 받아왔어요 = 받아왔어요 || {}, ... 널널한공간 .map( t => 범위로뽑기( t, 남은값 ) ) ) 
+	let [ 시작값 ] = 남은값 
+	시작값 && Object .assign( 받아왔어요, { [ 시작값 ] : 받아적어 } ) // remain original function 
 	Object .assign( 받아왔어요, ... 거시기 .map( ( v, i ) => 
 		({ [ 남은값[ i + 1 ] ] : ForV( v ) }) // extract one time 
-		) ); 
-	return `${ ForV( 시작값 ? 받아왔어요[ 시작값 ] : 받아적어, ... 거시기 ) }`; 
+		) ) 
+	return `${ ForV( 시작값 ? 받아왔어요[ 시작값 ] : 받아적어, ... 거시기 ) }` 
 	} 
-function 대략( ... ar ) { return { toString }; function toString() { // lazy loader with fix value .. 
-	return `${ Object .assign( this, 글자로뽑기( `${ 대충( ... ar ) }` ) ) }`; 
-	} } 
-function 대충( raw, ... ar ) { return { toString }; function toString() { // lazy loader with fix value .. 
-	var rawa = [], ara = []; 
-	raw .forEach( ( v, p ) => { 
-		let 
-			  [ ra, ... aa ] = ttoraw( v 
+function 대략( ... ar ) { return { toString } 
+	function toString() { // lazy loader with fix value .. 
+		return `${ Object .assign( this, 글자로뽑기( `${ 대충( ... ar ) }` ) ) }` 
+		} 
+	} 
+function 대충( raw, ... ar ) { 
+	return { toString } 
+	
+	function toString() { // lazy loader with fix value .. 
+		var rawa = [], ara = [] 
+		raw .forEach( ( v, p ) => { 
+			let [ ra, ... aa ] = ttoraw( v 
 				, /(?<꾸밈>[\s\S]*?)(?<치환곽>\{\s*(?<치환자>[\s\S]+?)\s*\}|$)/g 
 				, ( traw, tar ) => ( all, rawv, arv, arvv, origin, position, { 꾸밈, 치환곽, 치환자 } ) => ( 
 					  traw .push( 꾸밈 ) 
 					, tar .push( 치환곽 ? 받아왔어요 && 받아왔어요[ 치환자 ] || `{${ 치환자 }}` : '' ) 
 					) 
 				) 
-			, arp = ar[ p ] 
-			; 
-		rawa .push( ... ra ); 
-		ara .push( ... aa, '' ); // continue with next 
-		arp && ( 
-			  rawa .push( '' ) 
-			, ara .push( arp ) 
-			); 
-		} ) 
-		; 
-	ara .pop(); // cut about pack 
-	rawa .raw = rawa; 
-	
-	return ( t => this .toString = q => t )( String .raw( rawa, ... ara ) )(); 
-	} } 
-function 있는대로만( { raw }, ... ar ) { let printIndex = -1; return { 
-	  toString : q => 
-		String .raw( { raw }, ( 
-			  printIndex += 1 
-			, ar .map( a => a && a[ printIndex ] || '' ) 
-			) ) 
-	, sourceFrom : [ raw, ... ar ] 
-	}; } 
+			let arp = ar[ p ] 
+			rawa .push( ... ra ) 
+			ara .push( ... aa, '' ) // continue with next 
+			arp && ( 
+				  rawa .push( '' ) 
+				, ara .push( arp ) 
+				) 
+			} ) 
+		ara .pop() // cut about pack 
+		rawa .raw = rawa 
+		
+		return ( t => this .toString = q => t ) ( String .raw( rawa, ... ara ) )() 
+		} 
+	} 
+function 있는대로만( { raw }, ... ar ) { 
+	let printIndex = -1 
+	return { 
+		  toString : q => 
+			String .raw( { raw }, ( 
+				  printIndex += 1 
+				, ar .map( a => a && a[ printIndex ] || '' ) 
+				) ) 
+		, sourceFrom : [ raw, ... ar ] 
+		} 
+	} 
 function ttoraw( t, regv, regF ) { 
-	let raw = [], ar = []; 
-	t .replace( regv, regF( raw, ar ) ); 
-	let rpop, apop; 
+	let raw = [] 
+	let ar = [] 
+	t .replace( regv, regF( raw, ar ) ) 
+	let rpop, apop 
 	while ( [ rpop = raw .pop(), apop = ar .pop() ] .every( v => ! v ) && raw .length ) { 
 		} 
 	  apop ? ( raw .push( rpop, '' ), ar .push ( apop ) ) 
 	: rpop && ( raw .push( rpop ), ar .push ( apop ) ) 
-		; 
-	raw .raw = raw; 
-	raw .length ? ( ar .length = raw .length - 1 ) : raw .push( '' ); 
-	return [ raw, ... ar ]; 
+	
+	raw .raw = raw 
+	raw .length ? ( ar .length = raw .length - 1 ) : raw .push( '' ) 
+	return [ raw, ... ar ] 
 	} 
 function 글자로뽑기( t ) { 
 	return 있는대로만( ... ttoraw( t 
@@ -88,7 +95,7 @@ function 글자로뽑기( t ) {
 			  raw .push( 꾸밈 ) 
 			, ar .push( 돌돌이곽 && 돌돌이 .split( ',' ) ) 
 			) 
-		) ); 
+		) ) 
 	} 
 function 범위로뽑기( t, 남은값 ) { 
 	let [ raw, ... ar ] = ttoraw( t 
@@ -97,15 +104,15 @@ function 범위로뽑기( t, 남은값 ) {
 			  raw .push( 제목 ) 
 			, ar .push( 내용칸 && 내용 ) 
 			) 
-		); 
-	남은값 .push( raw[ ar .length ] ); 
+		) 
+	남은값 .push( raw[ ar .length ] ) 
 	return ar .length ? Object .assign( ... ar .map( ( t, p ) => 
 			({ [ raw[ p ] ] : 글자로뽑기( t ) }) ) 
 			) 
-		: {}; 
+		: {} 
 	} 
 function ForV( o, ... ar ) { 
-	return o instanceof Function ? { toString : q => `${ o( 받아왔어요, ... ar ) }` } : o; 
+	return o instanceof Function ? { toString : q => `${ o( 받아왔어요, ... ar ) }` } : o 
 	} 
 
 ///// 
