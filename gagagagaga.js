302b4a3edfb17cb3b608fc6916412b3a01dba1ed
killofki <killofki@yahoo.co.kr>
2020-02-08 11:13:25 +0900

    tabs

--- gagagagaga.js
+++ gagagagaga.js
@@ -1,16 +1,16 @@
 var { 이보게젊은이, 대략, 대충 } = initConst() 
 
 console .log( 이보게젊은이 ` 
-
+	
 	시작 ${ 대충 `{ 가 }{ 가 }{ 가 }{ 가 }{ 가 }?` } 
-
+	
 	가 ${ 대략 `[{ 그 아이 가 },{ 은는이 가 },{ 가씨네 가 },{ 성씨 가 },{ 인가요 가 }]` } 
 	그 아이 가 { [가〯 ] } 
 	은는이 가 { 가 } 
 	가씨네 가 { 가 } 
 	성씨 가 { 가〮 } 
 	인가요 가 { 가 } 
-
+	
 	` ) 
 // idea from https://twitter.com/Ranol__/status/1065972494060871680 
 // viral with https://twitter.com/killofki/status/1066035649134845952 
